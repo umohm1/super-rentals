0e8e874c6a0773932cf3f3b46e365faa329b68b5
Mary <umohm12@gmail.com>
2019-04-21 14:42:37 -0400

    Modify tests

--- tests/integration/components/list-filter-test.js
+++ tests/integration/components/list-filter-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, triggerKeyEvent, fillIn } from '@ember/tests-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 const ITEMS = [{city: 'San Francisco'}, {city: 'Portland'}, {city: 'Seattle'}];
@@ -11,6 +11,21 @@ module('Integration | Component | list-filter', function(hooks) {
 
   test('should initially load all listings', async function(assert) {
     this.set('filterByCity', () => Promise.resolve({results: ITEMS }));
+ 
+    test('should update with matching listings', async function (assert) {
+      this.set('filterByCity', (val) => {
+        if (val === '') {
+          return Promise.resolve({
+            query: val,
+            results: ITEMS
+          });
+        } else {
+          return Promise.resolve({
+            query: val,
+            results: FILTERED_ITEMS
+          });
+        }
+      });
 
     await render(hbs`
     <ListFilter @filter={{action filterByCity}} as |results|>
@@ -23,5 +38,10 @@ module('Integration | Component | list-filter', function(hooks) {
       </ul>
     </ListFilter>|
     `);
+
+    await fillIn(this.element.querySelector('.list-filter input'), 's');
+    await triggerKeyEvent(this.element.querySelector('.list-filter input'), "keyup", 83);
+
+    assert.equal(this.element.querySelectorAll('.city').length, 1, 'One result returned');
+    assert.equal(this.element.querySelector('.city').textContent.trim(), 'San Francisco');
   });
-});
